c164528bbbf917bd808a9259e9447540348d2429
eurowondollaryen <sehoakasayho@naver.com>
2022-11-23 21:39:21 +0900

    commonfunctions added

---
+++ src/common/commonFunctions.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import config from '../config.js';
+
+const commonFunctions = {
+    /*
+     * isNull
+     * Null check용 공통함수
+     * 
+     */
+    isNull: function(v) {
+        return (v === undefined || v === null || v === "") ? true : false;
+    },
+    /*
+     * getCommonCode
+     * Null check용 공통함수
+     */
+    getCommonCode: function(strCommCode) {
+        return axios.get(config.serverUrl + "/common-detail-code/search", {
+            params: {
+                COMM_CODE: strCommCode
+            }
+        });
+    },
+    /*
+     * getCommonCode
+     * Null check용 공통함수
+     */
+    getClientDate: function() {
+        let date = new Date();
+        let mm = date.getMonth() + 1; // getMonth() is zero-based
+        let dd = date.getDate();
+
+        return [date.getFullYear(),
+                (mm>9 ? '' : '0') + mm,
+                (dd>9 ? '' : '0') + dd
+                ].join('');
+    },
+    /*
+     * lookupValue
+     * get value with key (code) ex) 0001: "부서" => return "부서"
+     */
+    lookupValue: function(mappings, key) {
+        return mappings[key];
+    },
+    /*
+     * lookupKey
+     * get key with value (name) ex) 0001: "부서" => return "0001"
+     */
+    lookupKey: function(mappings, name) {
+        const keys = Object.keys(mappings);
+        for(let i = 0; i < keys.length; ++i) {
+            const key = keys[i];
+            if(mappings[key] === name) {
+                return key;
+            }
+        }
+        return "";
+    },
+    /*
+     * copyObject
+     * Object의 값을 copy하는 공통함수
+     */
+    copyObject: function(sourceObject, targetObject) {
+        if(!sourceObject) return null;
+        const keys = Object.keys(sourceObject);
+        keys.map((key) => {
+            targetObject[key] = sourceObject[key];
+        });
+        return targetObject;
+    }
+};
+
+export default commonFunctions;
